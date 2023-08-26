c17d85a7b723370051cd47283395d83abfd5173c
Daniel <dannybabbev@gmail.com>
2023-08-26 23:49:02 +0200

    to app directory

---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
+import { Ubuntu_Mono } from 'next/font/google'
+
+const ubuntuMono = Ubuntu_Mono({ 
+    subsets: ['latin'],
+    weight: '400',
+});
+
+export const metadata = {
+    icons: {
+      icon: '/favicon.ico',
+    },
+    title: 'BabbevOS',
+    description: 'Personal Website of Daniel Babbev',
+    openGraph: {
+      title: 'BabbevOS',
+      images: '/og-image.png',
+      description: 'Personal website of Daniel Babbev',
+    },
+}
+
+export default function RootLayout({
+    children,
+  }) {
+    return (
+      <html lang="en">
+        <body>
+            <main className={ubuntuMono.className}>
+                {children}
+            </main>
+            <Analytics />
+        </body>
+      </html>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
+'use client'
+
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Terminal from '../components/Terminal/Terminal';
 import IntroAnimation from '../components/IntroAnimation/IntroAnimation';
@@ -7,7 +8,7 @@ import { INTRO_TEXT } from '../helpers/consts';
 
 import BabbevOS from '../model/os';
 
-export default function Home() {
+export default function Page() {
   const os = new BabbevOS();
   const [displayIntroAnimation, setDisplayIntroAnimaion] = useState(true);
 
@@ -24,16 +25,6 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>BabbevOS</title>
-        <link rel="icon" href="/favicon.ico" />
-
-        <meta property="description" content="Personal Website of Daniel Babbev" />
-        <meta property="og:image" content="https://www.babbev.com/og-image.png"></meta>
-        <meta property="og:title" content="Daniel Babbev"></meta>
-        <meta property="og:description" content="Personal website" />
-      </Head>
-
       <div>
         { displayIntroAnimation && <IntroAnimation fadeOutAfter={beginFadeOutAfter} /> }
         { !displayIntroAnimation &&  
